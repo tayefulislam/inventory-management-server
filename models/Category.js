86a94f4994fcbe999494ef5f0523c16c1f6ae570
tayefulislam <kazitayefulislam12@gmail.com>
2022-10-09 22:56:31 +0600

    category model created

---
+++ models/Category.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const { ObjectId } = mongoose.Schema.Types;
+
+const CategorySchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Plase provide a category"],
+      maxLength: 100,
+      unique: true,
+      lowercase: true,
+    },
+    description: String,
+    imageURL: {
+      type: String,
+      validate: [validator.isUrl, "Please enter valid address"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Category = mongoose.model("Category", CategorySchema);
+
+module.exports = Category;
