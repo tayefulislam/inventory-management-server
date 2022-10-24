e3496d97ab2bcd4cdd1e80c5ff6951ac26d22259
tayefulislam <kazitayefulislam12@gmail.com>
2022-10-24 20:41:33 +0600

    Supplier service bug fix

--- Services/supplier.services.js
+++ Services/supplier.services.js
@@ -6,17 +6,19 @@ exports.createSupplierService = async (newStore) => {
 };
 
 exports.getSuppliersService = async () => {
-  const result = await Store.find({}).sort({ _id: -1 });
+  console.log("helo");
+  const result = await Supplier.find({}).sort({ _id: -1 });
+  console.log(result);
   return result;
 };
 
 exports.getSupplierByIDService = async (id) => {
-  const result = await Store.findById({ _id: id });
+  const result = await Supplier.findById({ _id: id });
   return result;
 };
 
 exports.updateSupplierByIdService = async (id, data) => {
-  const result = await Store.updateOne({ _id: id }, data, {
+  const result = await Supplier.updateOne({ _id: id }, data, {
     runValidators: true,
   });
   return result;
